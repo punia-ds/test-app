d3a5f120968a58483c331539aceecd29541119c0
umesh <umesh@thedreamsteps.com>
2024-02-27 12:25:42 +0530

    login setup

--- test-dashboard/src/app/app.component.ts
+++ test-dashboard/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  url = window.location.href;
+  path: any;
   public appPages = [
     { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
     { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
@@ -14,5 +17,11 @@ export class AppComponent {
     { title: 'Spam', url: '/folder/spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor(private ar: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    let pathArr = this.url.split('/');
+    this.path = pathArr[pathArr.length - 1];
+    console.log(this.path);
+  }
 }
