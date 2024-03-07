644328a251e8ff9a3cdc7637c5601dda544481ce
umesh <umesh@thedreamsteps.com>
2024-03-07 12:06:08 +0530

    dashboard screen

--- test-app/src/environments/environment.prod.ts
+++ test-app/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
   mandiApiKey: '579b464db66ec23bdd000001346e203f29274866504478eef12437af',
+  baseUrl: 'http://api.weareharyanvi.com/api/v1',
+  weatherApiKey: 'e34a124486b4a0dfeb72c5334ffa39bf',
 };
--- test-dashboard/src/environments/environment.prod.ts
+++ test-dashboard/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'http://api.weareharyanvi.com/api/v1',
 };
